aff552c20e2025195e0cf9e744b552db4eba2572
sacom1441 <samir.comas1@gmail.com>
2023-07-05 12:11:09 +0200

    sprint3 nivell1 exercici3

--- js/shop.js
+++ js/shop.js
@@ -69,7 +69,7 @@ let cartList = [];
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 const cart = [];
 
-const total = 0;
+let total = 0;
 
 // Exercise 1
 function buy(id) {
@@ -98,6 +98,13 @@ function cleanCart() {
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    let totalPrice = 0;
+
+    for(let i = 0; i < cartList.length; i++){
+        totalPrice += cartList[i].price;
+    }
+    console.log(totalPrice);
+    return(totalPrice);
 }
 
 // Exercise 4
