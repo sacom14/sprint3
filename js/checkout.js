160de3888f6d6424b5b398929644e33366995a1e
sacom1441 <samir.comas1@gmail.com>
2023-07-10 00:55:10 +0200

    sprint3 nivell1 exercici7 v.1

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,66 @@
 
-// Exercise 6
+// Exercise 7
 function validate() {
-	var error = 0;
+	const error = 0;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	const fName = document.getElementById("fName");
+	const fEmail = document.getElementById("fEmail");
+	const fAddress = document.getElementById("fAddress");
+	const fLastN = document.getElementById("fLastN");
+	const fPassword = document.getElementById("fPassword");
+	const fPhone = document.getElementById("fPhone");
+	
+	const form = document.querySelector(".form");
+
+
 
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");
+	const errorName = document.getElementById("errorName");
+	const errorEmail = document.getElementById("errorEmail");
+	const errorAddress = document.getElementById("errorAddress");
+	const errorLastN = document.getElementById("errorLastN");
+	const errorPassword = document.getElementById("errorPassword");
+	const errorPhone = document.getElementById("errorPhone");
 
-	// Validate fields entered by the user: name, phone, password, and email
-	if (fName.value == "") {
-		error++;
+
+	// Validate fields entered by the user: name,last name, address, phone, password, and email
+	let message = "";
+	// Name validation
+	if (fName.value.length < 3) {
+		message = "This field is required and must have, at least, 3 characters";
+		errorName.innerHTML = message;
+		console.log(message);
+		// error++;
+	}
+	// Email validation
+	if (fEmail.value.length < 3) {
+		// error++;
+	}
+	// Address validation
+	if (fAddress.value.length <3){
+
+	}
+	// Last Name validation
+	if (fLastN.value.length < 3){
+		
 	}
 
-	if (fEmail.value == "") {
-		error++;
+	// Password validation
+	if (fPassword.value.length <3){
+		
 	}
 
-	if (error > 0) {
-		alert("Error");
-	} else {
-		alert("OK");
+	// Phone validation
+	if (fPhone.value.length <3){
+		
 	}
+
+
+// esta prederminado
+	// if (error > 0) {
+	// 	alert("Error");
+	// } else {
+	// 	alert("OK");
+	// }
 
 }
